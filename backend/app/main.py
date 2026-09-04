aadfca7409377f67912ee6243f291cf7157871ba
pravansh <pravanshthombre20@gmail.com>
2026-09-04 10:05:03 +0530

    Fix HTML tags in UI and disable CORS credentials to allow profile creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -76,7 +76,7 @@ def startup_db_init():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
